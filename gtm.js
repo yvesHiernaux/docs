61ae8d26efa2cdb4abe6970ac5f268634e6c4db2
Yves Hiernaux <yves@beebole.com>
2025-06-04 16:28:12 +0200

    Create gtm.js

---
+++ gtm.js
@@ -0,0 +1,1 @@
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://load.pollen.beelit.com/elxxvtjls.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','10=aWQ9R1RNLTVLWjMyMjNO&page=2');
